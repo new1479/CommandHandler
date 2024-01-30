1d28d1982cb7b124d3d2f41aab82f147d22501ad
new1479 <jbidi70@gmail.com>
2024-01-30 23:28:02 +0000

    commit main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CommandHandler
+- Allows you to have multiple arguments and aliases
+- Fairly simple. Designed for noobs, and also so that only you guys can make it even more slower than it already is. haha
---
+++ main.lua
@@ -0,0 +1,50 @@
+
+local cmds = {}
+
+local pf: string = "!"
+
+local lp: userdata = game:GetService("Players").LocalPlayer
+
+local addcmd = function(names: {string}, func: (args: {any}) -> void)
+
+    --[[ 
+        assert(type(names) == "table")
+        assert(type(func) == "function")
+    ]]
+
+    for i, _ in next, names do
+        if not cmds[names[i]] then
+            cmds[names[i]] = func
+        end
+    end
+end
+
+-- examples
+addcmd({"hello", "greet", "sayhello"}, function(args)
+    print("Hello, my name is " ..args[1])
+end)
+
+addcmd({"args"}, function(args)
+    for i = 1, 10 do
+        print(args[i])
+    end
+end)
+
+
+lp.Chatted:Connect(function(msg)
+    local args: {string} = msg:lower():split(" ")
+
+    if string.sub(args[1], 1, 1) ~= pf then 
+        return 
+    end
+
+    local check = string.sub(args[1], #pf + 1, #args[1])
+
+    if not cmds[check] then
+         return 
+    end
+
+    table.remove(args, 1)
+
+    cmds[check](args)
+end)
